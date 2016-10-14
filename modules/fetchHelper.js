f7ada655c2ff1cc353dcb210c6caf294190fc861
Kidel <tano.design@gmail.com>
2016-10-14 23:48:24 +0200

    more optimization (dom function is calculated only once per url) and cleaning of some console logs

--- modules/fetchHelper.js
+++ modules/fetchHelper.js
@@ -32,7 +32,6 @@ var fetchHelper = {
     },
 
     sanitize: function(html) {
-        console.log("sanitizing");
         var body = sanitizeHtml(html, {
             allowedTags: ['a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article', 'aside', 'audio', 'b', 'base', 'basefont', 'bdi', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'dir', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'keygen', 'label', 'legend', 'li', 'link', 'main', 'map', 'mark', 'menu', 'menuitem', 'meta', 'meter', 'nav', 'noframes', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's', 'samp', 'section', 'select', 'small', 'source', 'span', 'strike', 'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'tt', 'u', 'ul', 'var', 'video', 'wbr'],
             allowedAttributes: false
@@ -41,25 +40,21 @@ var fetchHelper = {
         return body;
     },
 
-    applyXPath: function(body, path, callback) {
+    applyXPath: function(doc, path, callback) {
         console.log("applying Xpath: " + path);
-        var doc = new dom().parseFromString(body);
         var nodes = xpath.select(path, doc);
         console.log("done, " + nodes.length + " results");
         callback(nodes);
     },
 
     applyEveryPath: function(url, body, paths, callback) {
+        var doc = new dom().parseFromString(body);
         var results = [];
         var keys = Object.keys(paths);
-        console.log(keys);
-
         keys.forEach(function(key, j, array) {
             if (paths[key] == null) return; //in array.forEach it's the same as 'continue' for a normal for
             var path = paths[key].trim().replace(/(\r\n|\n|\r)/gm, "");
-            fetchHelper.applyXPath(body, path, function (nodes) {
-                console.log("path: " + path);
-                console.log("returning from calls");
+            fetchHelper.applyXPath(doc, path, function (nodes) {
                 if (typeof nodes != 'undefined' && typeof nodes[0] != 'undefined') {
                     //console.log(nodes[0].localName + ": " + nodes[0].firstChild.data);
                     console.log("node: " + nodes[0].toString());
